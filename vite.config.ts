20e7792849aec8550dcc5d869e94bcea3b152e03
Robozonics <rehan28052009@gmail.com>
2026-09-26 23:36:18 +0530

    fix: resolve utf-16 encoding bug in env file causing dotenv parse failure

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
-// forced restart
+// forced restart 2
 export default defineConfig({
   plugins: [
     react(),
