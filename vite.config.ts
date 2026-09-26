ebcdbc711124688ffbd91ddb6e80da8bad13933b
Robozonics <rehan28052009@gmail.com>
2026-09-26 23:32:49 +0530

    chore: trigger vite hard restart

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
+// forced restart
 export default defineConfig({
   plugins: [
     react(),
